757c5588154f8ac0a0c4fd9814cb1b5d1a4f357e
Zafar Md Imam <zmimam@uei.com>
2018-11-21 14:28:17 +0530

    New file added

---
+++ modulea.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 14:23:22 2018
+
+@author: zmimam
+"""
+
+class A:
+    
+    def __init__(self):
+        print('Class A initialized')
+    
+    def display_from_a(self):
+        print('Display message A')
+
+a = A()
+a.display_from_a()
